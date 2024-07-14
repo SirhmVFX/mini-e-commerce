72446c063d4e527f9e6de1684ff36d90afa11577
Fullstack Mechanic <fullstack@Fullstacks-MacBook-Air.local>
2024-07-14 02:50:14 +0100

    push

--- components/AllProducts.jsx
+++ components/AllProducts.jsx
@@ -83,10 +83,8 @@ function AllProducts() {
     indexOfLastProduct,
   );
 
-  // Calculate total number of pages
   const totalPages = Math.ceil(allProducts?.length / postperpage);
 
-  // Generate an array of page numbers
   const pageNumbers = [];
   for (let i = 1; i <= totalPages; i++) {
     pageNumbers.push(i);
